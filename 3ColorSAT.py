b6d42cd6ac92557d0d0b0642efed978996ade013
Michael <michaelnotbolton@gmail.com>
2020-06-17 15:01:04 -0500

    updated 3ColorSAT

--- 3ColorSAT.py
+++ 3ColorSAT.py
@@ -22,5 +22,39 @@ def number_of_clauses():
     #4 clauses for every edge
     n = 4*number_of_vars
     # how many clauses for each vertex?
-    
+
     return n
+    
+variable_count = 0
+#compile CNF piece by piece
+def create_cnf():
+    str_cnf = ""
+    str_cnf += comments()
+    str_cn += pline()
+    str_cnf += "c Edge Clauses \n"
+    str_cn += edges_clauses()
+    return str_cnf
+
+def comments():
+    str_comments = "\
+    c Practice CNF"
+    return str_comments
+
+#construct p as p + type + #variable + #clauses
+def pline():
+    str_pline = "p cnf "
+    str_pline += str(number_of_vars()) + " "
+    str_pline += str(number_of_clauses()) + "\n"
+    return str_pline
+
+def edges_clauses():
+    edge_clause = ""
+    for edge in edges:
+        variable_count = c
+        map = {a:c+1,b:c+2,c:c+3}
+        for key in map.keys():
+            for k in map.keys():
+                if k != key:
+                    edge_clause += "-" str(k)
+                else
+        
