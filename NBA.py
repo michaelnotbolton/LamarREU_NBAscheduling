82299fefadc67bb1c18d77b8f60e2bbc6543e7e5
Michael <michaelnotbolton@gmail.com>
2020-06-22 15:09:34 -0500

    Add NBA.py functionality

--- NBA.py
+++ NBA.py
@@ -31,26 +31,53 @@ lst_conferences = ["West","East"]
 lst_divisions = ["Atlantic","Central","Northwest","Pacific","Southeast","Southwest"]
 
 class NBA:
+    ''' A structure for NBA organization '''
     def __init__(self):
         name_file = open(os.getcwd() + "\\teams.csv","r")
         self.lst_teams = []
         self.lst_w_teams = []
         self.lst_e_teams = []
+        self.dict_divisions = {}
         for line in name_file:
             teaminfo = line.split(",")
             teaminfo[4] = teaminfo[4][0:len(teaminfo[4])-1]
             team = Team(teaminfo[4],teaminfo[3],teaminfo[2],teaminfo[1],teaminfo[0])
             self.lst_teams.append(team)
-            if team.get_conference == "West":
+            if team.get_conference() == "West":
                 self.lst_w_teams.append(team)
+                self.
             else:
                 self.lst_e_teams.append(team)
+            if team.get_division() in self.dict_divisions.keys():
+                self.dict_divisions[team.get_division()].append(team)
+            else:
+                self.dict_divisions.update({team.get_division():team})
 
-    def west(self):
+    def west_teams(self):
         return self.lst_w_teams
 
-    def east(self):
+    def east_list(self):
         return self.lst_e_teams
+    
+    def division_list(self,div):
+        return self.dict_divisions[div]
+    
+    def teams(self):
+        return self.lst_teams
+    
+    def west_divisions(self):
+        lst_divs = []
+        for team in self.lst_teams:
+            if team.get_conference()="West":
+                lst_divs.append(team)
+        return lst_divs
+    
+    def east_divisions(self):
+        lst_divs = []
+        for team in self.lst_teams:
+            if team.get_conference()="East":
+                lst_divs.append(team)
+        return lst_divs
 
 
 
@@ -59,5 +86,5 @@ league = NBA()
 print(league.west())
 
 for team in league.west():
-    print("done")
+    #print("done")
     print(team.get_conference())
