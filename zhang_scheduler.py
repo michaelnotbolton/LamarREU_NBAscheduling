f8e00969b2249208d96ad0a51c2a2da24b8c96b0
ram112358 <rammoore112358@gmail.com>
2020-07-15 15:26:17 -0400

    added some comments and dictionary lookup

--- zhang_scheduler.py
+++ zhang_scheduler.py
@@ -13,14 +13,14 @@ east_codes = [team.get_code() for team in nba.east_teams()]
 
 var_list = []
 
+#this generates our list of variables
 for home_team in codes: # for each home team
-    codes_sans_home = copy.copy(codes)
-    codes_sans_home.remove(home_team)
+    codes_sans_home = copy.copy(codes) # copy the set of teams
+    codes_sans_home.remove(home_team) # and remove the home team, teams don't play themselves
     for away_team in codes_sans_home: # for each away team/
         for day in range(180): # for each day
             var_list.append(home_team+"_"+away_team+"_"+str(day))
-
-
+var_dict = {var:index for index,var in var_list} # instead of using linear time var_list.index() we can use a constat time dictionary lookup
 
 
 clause_list = []
@@ -28,26 +28,26 @@ clause_list = []
 def create_clauses():
     global clause_list
 
-def day_exclusion_clauses(daynum):
+def day_exclusion_clauses(team_codes, daynum): # ensures that the teams in the teams list don't play on the day, daynum
     clauses = []
-    for home_team in codes: # for each home team
-        codes_sans_home = copy.copy(codes)
-        codes_sans_home.remove(home_team)
-        for away_team in codes_sans_home: # for each away team
-            clauses.append(str(-var_list.index(home_team+"_"+away_team+"_"+str(daynum))))
+    for home_team in team_codes: # for each home team
+        team_codes_sans_home = copy.copy(team_codes) # copy the set of teams
+        team_codes_sans_home.remove(home_team) # and remove the home team, teams don't play themselves
+        for away_team in team_codes_sans_home: # for each away team
+            clauses.append(str(-var_dict[home_team+"_"+away_team+"_"+str(daynum)])) # a clause of just var ensures that it is false in a solution
     return clauses
 
-def conference_clauses():
+def interconference_clauses(): # each team plays two games against every team in the other conference, one home and one away
     clauses = []
-    for west_team in west_codes:
-        for east_team in east_codes:
-            home_pairing_var_set = []
-            away_pairing_var_set = []
-            for day in range(180):
-                home_pairing_var_set.append(var_list.index(west_team+"_"+east_team+"_"+str(day)))
-                away_pairing_var_set.append(var_list.index(east_team+"_"+west_team+"_"+str(day)))
-            clauses.extend(true_literal_equals_clause(home_pairing_var_set,1))
-            clauses.extend(true_literal_equals_clause(away_pairing_var_set,1))
+    for west_team in west_codes: # built from the point of view of the western conference
+        for east_team in east_codes: # every team in the other conference
+            home_pairing_var_set = [] # the western team's home game for each interconference pairing
+            away_pairing_var_set = [] # the western team's away game for each interconference pairing
+            for day in range(180): # populating the arrays for each day of the season
+                home_pairing_var_set.append(var_dict[west_team+"_"+east_team+"_"+str(day)])
+                away_pairing_var_set.append(var_dict[east_team+"_"+west_team+"_"+str(day)])
+            clauses.extend(true_literal_equals_clause(home_pairing_var_set,1)) # each interconference pairing plays exactly one home game for the western conference team
+            clauses.extend(true_literal_equals_clause(away_pairing_var_set,1)) # each interconference pairing plays exactly one away game for the western conference team
     return clauses
 
 def one_game_per_team_per_day_clauses():
@@ -58,8 +58,8 @@ def one_game_per_team_per_day_clauses():
         for day in range(180):
             team_day_var_set = []
             for other_team in codes_sans_one:
-                team_day_var_set.append(var_list.index(team+"_"+other_team+"_"+str(day)))
-                team_day_var_set.append(var_list.index(other_team+"_"+team+"_"+str(day)))
+                team_day_var_set.append(var_dict[team+"_"+other_team+"_"+str(day)])
+                team_day_var_set.append(var_dict[other_team+"_"+team+"_"+str(day)])
             clauses.extend(true_literal_leq_clause(team_day_var_set,1))
     return clauses
 
@@ -87,7 +87,7 @@ def true_literal_leq_clause(n_vars,k):
     return clauses
 
 
-print(len(conference_clauses()))
+print(len(interconference_clauses()))
 # print(len(one_game_per_team_per_day_clauses()))
 # print(true_equals_literal_clause(["1","2","3","4","5"],3))
 # print(day_exclusion_clauses(123))
